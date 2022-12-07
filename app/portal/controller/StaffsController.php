fce0a15116dc636aba83f0417094f793e0db7132
PC-20221013ZCHV\Administrator <755615933@qq.com>
2022-12-07 18:25:08 +0800

    1

--- app/portal/controller/StaffsController.php
+++ app/portal/controller/StaffsController.php
@@ -4,6 +4,7 @@ namespace app\portal\controller;
 use \cmf\controller\HomeBaseController;
 class StaffsController extends HomeBaseController
 {
+    
     public function index()
     {
         return $this->fetch(':staffs');
