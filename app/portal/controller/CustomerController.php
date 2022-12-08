5842c3bfe3eb7528537daa13aa1d08d29f62fbc6
PC-20221013ZCHV\Administrator <755615933@qq.com>
2022-12-08 17:47:41 +0800

    1

--- app/portal/controller/CustomerController.php
+++ app/portal/controller/CustomerController.php
@@ -1,13 +1,18 @@
 <?php
 namespace app\portal\controller;
 
+use app\portal\service\PostService;
 use \cmf\controller\HomeBaseController;
 class CustomerController extends HomeBaseController{
 	
 	public function index(){
+        $postService = new PostService();
+        $arr=['category'=>29];
+        $data        = $postService->adminArticleList($arr);
+        $this->assign("list", $data);
 		return $this->fetch(":customer");
 	}
-	
+
 }
 
 
