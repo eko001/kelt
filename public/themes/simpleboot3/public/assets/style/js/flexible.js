46b7f41f0687dc0fb86adcddf02e3405d9ed78bf
wanglili <1141824003@qq.com>
2022-12-08 18:02:12 +0800

    1

--- public/themes/simpleboot3/public/assets/style/js/flexible.js
+++ public/themes/simpleboot3/public/assets/style/js/flexible.js
@@ -1,4 +1,4 @@
-(function(win, lib) {
+(function (win, lib) {
     var doc = win.document;
     var docEl = doc.documentElement;
     var metaEl = doc.querySelector('meta[name="viewport"]');
@@ -7,7 +7,7 @@
     var scale = 0;
     var tid;
     var flexible = lib.flexible || (lib.flexible = {});
-     
+
     if (metaEl) {
         console.warn('将根据已有的meta标签来设置缩放比例');
         var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
@@ -22,11 +22,11 @@
             var maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
             if (initialDpr) {
                 dpr = parseFloat(initialDpr[1]);
-                scale = parseFloat((1 / dpr).toFixed(2));   
+                scale = parseFloat((1 / dpr).toFixed(2));
             }
             if (maximumDpr) {
                 dpr = parseFloat(maximumDpr[1]);
-                scale = parseFloat((1 / dpr).toFixed(2));   
+                scale = parseFloat((1 / dpr).toFixed(2));
             }
         }
     }
@@ -36,9 +36,9 @@
         var devicePixelRatio = win.devicePixelRatio;
         if (isIPhone) {
             // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {               
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
                 dpr = 3;
-            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
                 dpr = 2;
             } else {
                 dpr = 1;
@@ -62,20 +62,25 @@
             doc.write(wrap.innerHTML);
         }
     }
-    function refreshRem(){
+    function refreshRem() {
         var width = docEl.getBoundingClientRect().width;
         if (width / dpr > 540) {
-            width = 540 * dpr;
+            width = width * dpr;
         }
-        var rem = width / 10;
+        if (width / dpr > 1440) {
+            width = 1400 * dpr;
+        }
+        var rem = width / 25;
+		console.log('rem',rem)
+		console.log('width',width)
         docEl.style.fontSize = rem + 'px';
         flexible.rem = win.rem = rem;
     }
-    win.addEventListener('resize', function() {
+    win.addEventListener('resize', function () {
         clearTimeout(tid);
         tid = setTimeout(refreshRem, 300);
     }, false);
-    win.addEventListener('pageshow', function(e) {
+    win.addEventListener('pageshow', function (e) {
         if (e.persisted) {
             clearTimeout(tid);
             tid = setTimeout(refreshRem, 300);
@@ -84,22 +89,22 @@
     if (doc.readyState === 'complete') {
         doc.body.style.fontSize = 12 * dpr + 'px';
     } else {
-        doc.addEventListener('DOMContentLoaded', function(e) {
+        doc.addEventListener('DOMContentLoaded', function (e) {
             doc.body.style.fontSize = 12 * dpr + 'px';
         }, false);
     }
-     
+
     refreshRem();
     flexible.dpr = win.dpr = dpr;
     flexible.refreshRem = refreshRem;
-    flexible.rem2px = function(d) {
+    flexible.rem2px = function (d) {
         var val = parseFloat(d) * this.rem;
         if (typeof d === 'string' && d.match(/rem$/)) {
             val += 'px';
         }
         return val;
     }
-    flexible.px2rem = function(d) {
+    flexible.px2rem = function (d) {
         var val = parseFloat(d) / this.rem;
         if (typeof d === 'string' && d.match(/px$/)) {
             val += 'rem';
