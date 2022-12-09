0475297f79091d406c32f83891399b977deef999
PC-20221013ZCHV\Administrator <755615933@qq.com>
2022-12-09 13:55:55 +0800

    1

--- app/portal/controller/SolutionController.php
+++ app/portal/controller/SolutionController.php
@@ -8,14 +8,7 @@ use cmf\controller\HomeBaseController;
 class SolutionController extends HomeBaseController
 {
     public function index(){
-		$id= $this->request->param('id', 0, 'intval');
-		if($id == 1){
-			return $this->fetch(':solution1');
-		}else if($id == 2){
-			return $this->fetch(':solution2');
-		}else if($id == 3){
-			return $this->fetch(':solution3');
-		} 
+		return $this->fetch(':solution');
     }
 
     public function detail(){
